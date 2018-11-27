83fadef33e25fa7de4c865d16625768450bb85f8
DeuterGraves <carolinetgraves@gmail.com>
2018-11-27 23:37:28 +0000

    tabacco stalls does not admit under 18s

--- src/main/java/Stalls/TabaccoStall.java
+++ src/main/java/Stalls/TabaccoStall.java
@@ -1,8 +1,25 @@
 package Stalls;
 
-public class TabaccoStall extends Stall{
+import ThemePark.ISecurity;
+import ThemePark.Visitor;
+
+public class TabaccoStall extends Stall implements ISecurity {
+
+    private int minAge;
 
     public TabaccoStall(String name, String ownerName, String parkingSpot) {
         super(name, ownerName, parkingSpot);
+        this.minAge = 18;
+    }
+
+    public int getMinAge(){
+        return this.minAge;
+    }
+
+    public boolean isAllowedTo(Visitor visitor) {
+        if (visitor.getAge() >= this.minAge){
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/Stalls/TabaccoStallTest.java
+++ src/test/java/Stalls/TabaccoStallTest.java
@@ -1,5 +1,6 @@
 package Stalls;
 
+import ThemePark.Visitor;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,10 +9,14 @@ import static org.junit.Assert.assertEquals;
 public class TabaccoStallTest {
 
     TabaccoStall tabaccoStall;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
     public void before(){
         tabaccoStall = new TabaccoStall("The Cuddle Up!", "Barry Tarhampton", "1A");
+        visitor1 = new Visitor("Della Pandalan", 20, 120, 15);
+        visitor2 = new Visitor("Robert Bingston", 16, 167, 25);
     }
 
     @Test
@@ -27,5 +32,20 @@ public class TabaccoStallTest {
     @Test
     public void tabaccoStallHasParkingSpot(){
         assertEquals("1A", tabaccoStall.getParkingSpot());
+    }
+
+    @Test
+    public void tabaccoStallHasMinAge(){
+        assertEquals(18, tabaccoStall.getMinAge());
+    }
+
+    @Test
+    public void tabaccoStallRejectsUnder18s(){
+        assertEquals(false, tabaccoStall.isAllowedTo(visitor2));
+    }
+
+    @Test
+    public void tabaccoStallAllowsOver18s(){
+        assertEquals(true, tabaccoStall.isAllowedTo(visitor1));
     }
 }
