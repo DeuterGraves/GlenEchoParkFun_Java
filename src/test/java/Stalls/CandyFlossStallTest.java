641e447ebcc7fe6769833f12669c4f1af1c422b4
DeuterGraves <carolinetgraves@gmail.com>
2018-11-27 22:51:16 +0000

    candy floss stall up and tested

---
+++ src/test/java/Stalls/CandyFlossStallTest.java
@@ -0,0 +1,31 @@
+package Stalls;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CandyFlossStallTest {
+
+    CandyFlossStall candyFlossStall;
+
+    @Before
+    public void before(){
+        candyFlossStall = new CandyFlossStall("Tooth Rot!", "Lætitia Sugarinham", "A1");
+    }
+
+    @Test
+    public void candFlossStallhasName(){
+        assertEquals("Tooth Rot!", candyFlossStall.getName());
+    }
+
+    @Test
+    public void candyFlossStallHasOwner(){
+        assertEquals("Lætitia Sugarinham", candyFlossStall.getOwnerName());
+    }
+    
+    @Test
+    public void candyFlossStallHasParkingSpot(){
+        assertEquals("A1", candyFlossStall.getParkingSpot());
+    }
+}
