220fac84d11b37fef495d28cfde759584b57d3a0
DeuterGraves <carolinetgraves@gmail.com>
2018-11-27 16:58:24 +0000

    basic themepark project set up

---
+++ src/main/java/Visitor.java
@@ -0,0 +1,30 @@
+public class Visitor {
+
+    private String name;
+    private int age;
+    private int height;
+    private int wallet;
+
+    public Visitor(String name, int age, int height, int wallet){
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.wallet = wallet;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public int getWallet() {
+        return this.wallet;
+    }
+}
---
+++ src/test/java/VisitorTest.java
@@ -0,0 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class VisitorTest {
+
+    Visitor visitor;
+
+    @Before
+    public void before(){
+        visitor = new Visitor("Helen Abernathy", 13, 147, 25);
+    }
+
+    @Test
+    public void visitorHasName(){
+        assertEquals("Helen Abernathy", visitor.getName());
+    }
+
+    @Test
+    public void visitorHasAge(){
+        assertEquals(13, visitor.getAge());
+    }
+
+    @Test
+    public void visitorHasHeight(){
+        assertEquals(147, visitor.getHeight());
+    }
+
+    @Test
+    public void visitorHasMoney(){
+        assertEquals(25, visitor.getWallet());
+    }
+}
